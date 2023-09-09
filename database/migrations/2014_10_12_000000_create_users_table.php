8e3c5b9f35c76e798015624eeefa73cec243618f
moatazalshabuo <king.rab99@gmail.com>
2023-09-09 17:50:32 -0400

    all edit

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,11 +13,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name',20);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('username')->unique();
+            $table->string('username',30)->unique();
             $table->rememberToken();
             $table->timestamps();
         });
